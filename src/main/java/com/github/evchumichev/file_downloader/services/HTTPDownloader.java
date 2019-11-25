b8536a043e91c0e14582eb3b78eb0f14600f893a
Evgenii Chumichev <evchumichev@gmail.com>
2019-11-25 21:17:38 +0700

    HTTPDownloader can resume download

--- src/main/java/com/github/evchumichev/file_downloader/services/HTTPDownloader.java
+++ src/main/java/com/github/evchumichev/file_downloader/services/HTTPDownloader.java
@@ -4,15 +4,15 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-
 
 public class HTTPDownloader implements Runnable {
+    private static final int RANGE_NOT_SATISFIABLE = 416;
     private FileCreator fileCreator;
     private String fileURL;
     private String filePath;
-    private URLConnection connection;
+    private HttpURLConnection connection;
 
     public HTTPDownloader(String url, String path) {
         fileURL = url;
@@ -28,18 +28,23 @@ public class HTTPDownloader implements Runnable {
     private void downloadFile() {
         try {
             File file = fileCreator.create(fileURL, filePath);
-            connection = new URL(fileURL).openConnection();
-            try (BufferedInputStream stream = new BufferedInputStream(connection.getInputStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-                byte[] dataBuffer = new byte[1024];
-                int bytes;
-                while ((bytes = stream.read(dataBuffer, 0, 1024)) != -1) {
-                    fileOutputStream.write(dataBuffer, 0, bytes);
-                }
+            connection = (HttpURLConnection) new URL(fileURL).openConnection();
+            if (file.exists()) {
+                connection.setRequestProperty("Range", "bytes=" + file.length() + "-");
             }
-
+            if (connection.getResponseCode() == HTTPDownloader.RANGE_NOT_SATISFIABLE) {
+                throw new IOException(String.format("File %s already downloaded!", file.getName()));
+            }
+                try (BufferedInputStream stream = new BufferedInputStream(connection.getInputStream());
+                     FileOutputStream fileOutputStream = new FileOutputStream(file, true)) {
+                    byte[] dataBuffer = new byte[1024];
+                    int bytes;
+                    while ((bytes = stream.read(dataBuffer, 0, 1024)) != -1) {
+                        fileOutputStream.write(dataBuffer, 0, bytes);
+                    }
+                }
         } catch (IOException | RuntimeException e) {
-            e.printStackTrace();
+            System.out.println(e.getLocalizedMessage());
         }
     }
 }
